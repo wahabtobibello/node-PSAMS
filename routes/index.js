1e2693afda10c3a0bdfb82bfccbd8b2c969f22a1
Tobi Bello <bello.tobi@outlook.com>
2017-11-08 23:29:10 +0100

    bootstrapped login and register routes

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,26 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router
+  .route("/login")
+  .get(function (req, res, next) {
+    res.render('login');
+  })
+  .post(function (req, res, next) {
+    res.send(req.body);
+  });
+
+router
+  .route("/register")
+  .get(function (req, res, next) {
+    res.render('register');
+  })
+  .post(function (req, res, next) {
+    res.send(req.body);
+  });
 
 module.exports = router;
