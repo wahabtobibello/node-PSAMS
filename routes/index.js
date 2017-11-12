8b29091edd6d9787378965aee7bdd76011dec921
Tobi Bello <bello.tobi@outlook.com>
2017-11-12 19:56:39 +0100

    added routes: - logout - test used csrf implementation

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,11 @@
 var express = require('express');
+var jwt = require('jsonwebtoken');
+
+var Student = require("../models/Student");
+var Supervisor = require("../models/Supervisor");
+var mid = require("../middlewares");
+var sendErrorMessage = require("../helpers").sendErrorMessage;
+
 var router = express.Router();
 
 /* GET home page. */
@@ -9,7 +16,7 @@ router.get('/', function (req, res, next) {
 router
   .route("/login")
   .get(function (req, res, next) {
-    res.render('login');
+    res.render('login', { csrfToken: req.csrfToken() });
   })
   .post(function (req, res, next) {
     res.send(req.body);
@@ -18,10 +25,28 @@ router
 router
   .route("/register")
   .get(function (req, res, next) {
-    res.render('register');
+    res.render('register', { csrfToken: req.csrfToken() });
   })
   .post(function (req, res, next) {
     res.send(req.body);
   });
+router.get("/logout", function (req, res, next) {
+  req.session = null;
+  res.redirect("/login");
+
+})
+router.get("/test", function (req, res, next) {
+  // console.dir(req)
+
+  // Supervisor.findOne({
+  //   staffNumber: 123456789
+  // }).exec(function (err, supervisor) {
+  //   if (err) return next(err);
+  //   res.send(supervisor);
+  // })
+
+  // req.flash('danger', 'hello!');
+  // res.render("register");
+})
 
 module.exports = router;
