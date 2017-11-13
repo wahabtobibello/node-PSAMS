7e760f9b1714f6a5e75a6fc53abfa0e4f88b8738
Tobi Bello <bello.tobi@outlook.com>
2017-11-13 16:07:46 +0100

    loggedOut middleware added

--- middlewares/index.js
+++ middlewares/index.js
@@ -4,13 +4,20 @@ function loggedIn(req, res, next) {
   if (isLoggedIn) {
     return next();
   }
-    req.session = null;
+  req.session = null;
   req.flash("danger", "You must be logged In");
-    res.redirect("/login");
-  } 
-  res.locals.isAuthenticated = true;
-  next();
+  res.redirect("/login");
+}
+
+function loggedOut(req, res, next) {
+  var { isLoggedIn } = res.locals;
+  if (!isLoggedIn) {
+    return next();
+  }
+  req.flash("danger", "Logged out first");
+  res.redirect("/");
 }
 
 
-module.exports.requireAuth = requireAuth;
+module.exports.loggedIn = loggedIn;
+module.exports.loggedOut = loggedOut;
