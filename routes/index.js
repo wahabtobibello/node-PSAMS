5875efe9c0ba557a2f46c302ac17712331704334
Tobi Bello <bello.tobi@outlook.com>
2017-11-13 16:17:11 +0100

    implemented login

--- routes/index.js
+++ routes/index.js
@@ -19,16 +19,90 @@ router
     res.render('login', { csrfToken: req.csrfToken() });
   })
   .post(function (req, res, next) {
-    res.send(req.body);
+    var {
+      matricNumber,
+      password
+    } = req.body;
+    Student.findOne({ matricNumber })
+      .exec(function (err, student) {
+        if (err) return next(err);
+        if (!student) {
+          sendErrorMessage(req, res, "login", "User not found", { csrfToken: req.csrfToken() });
+          return;
+        }
+        student.verifyPassword(password)
+          .then(function (valid) {
+            if (!valid) {
+              sendErrorMessage(req, res, "login", "Username and Password Mismatch", { csrfToken: req.csrfToken() })
+              return;
+            }
+            var token = jwt.sign({
+              exp: Math.floor(Date.now() / 1000) + 3600,
+              nbf: Math.floor(Date.now() / 1000),
+              iss: req.baseUrl,
+              sub: student._id,
+              iat: Math.floor(Date.now() / 1000),
+              is_admin: student.role === "Supervisor"
+            }, process.env.SECRET_KEY);
+            req.session.accessToken = token;
+            res.redirect("/");
+          })
+          .catch(function (err) {
+            next(err);
+          });
+      })
   });
 
 router
   .route("/register")
-  .get(function (req, res, next) {
+  .get(middleware.loggedOut, function (req, res, next) {
     res.render('register', { csrfToken: req.csrfToken() });
   })
   .post(function (req, res, next) {
-    res.send(req.body);
+    var {
+      firstName,
+      lastName,
+      matricNumber,
+      password,
+      confirmPassword,
+    } = req.body;
+
+    if (password != confirmPassword) {
+      sendErrorMessage(req, res, "register", "Passwords don't match", { firstName, lastName, matricNumber, csrfToken: req.csrfToken() });
+      return;
+    }
+
+    Student.findOne({
+      matricNumber
+    }).exec(function (err, student) {
+      if (err) return next(err);
+      if (student) {
+        sendErrorMessage(req, res, "register", "User Already Exists", { firstName, lastName, matricNumber, csrfToken: req.csrfToken() })
+        return;
+      }
+      var newStudentRegistration = new Student({
+        name: {
+          first: firstName,
+          last: lastName
+        },
+        password,
+        matricNumber
+      });
+      Supervisor.findOne({
+        staffNumber: 123456789
+      }).exec(function (err, supervisor) {
+        if (err) return next(err);
+        newStudentRegistration.supervisor = supervisor
+        newStudentRegistration.save(function (err, student) {
+          if (err) {
+            console.error(err);
+            return next(err);
+          }
+          req.flash("info", "You can now log in")
+          res.redirect("/login");
+        });
+      });
+    })
   });
 router.get("/logout", function (req, res, next) {
   req.session = null;
@@ -49,17 +123,17 @@ router.get("/logout", function (req, res, next) {
 //   return res.send(sup);
 // });
 
-  // console.dir(req)
+// console.dir(req)
 
-  // Supervisor.findOne({
-  //   staffNumber: 123456789
-  // }).exec(function (err, supervisor) {
-  //   if (err) return next(err);
-  //   res.send(supervisor);
-  // })
+// Supervisor.findOne({
+//   staffNumber: 123456789
+// }).exec(function (err, supervisor) {
+//   if (err) return next(err);
+//   res.send(supervisor);
+// })
 
-  // req.flash('danger', 'hello!');
-  // res.render("register");
+// req.flash('danger', 'hello!');
+// res.render("register");
 // })
 
 module.exports = router;
