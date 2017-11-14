3b8ffe9c13fb1ce81025e04d31cfcc1691654ed7
Tobi Bello <bello.tobi@outlook.com>
2017-11-14 10:33:41 +0100

    moved middlewares to folder

--- middlewares/index.js
+++ middlewares/index.js
@@ -40,7 +40,24 @@ function loggedOut(req, res, next) {
 	res.redirect("/");
 }
 
+function notFoundHandler(req, res, next) {
+	var err = new Error("Not Found");
+	err.status = 404;
+	next(err);
+}
+
+function errorHandler(err, req, res) {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get("env") === "development" ? err : {};
+
+	// render the error page
+	res.status(err.status || 500);
+	res.render("error");
+}
 
 module.exports.setUserCredentials = setUserCredentials;
 module.exports.loggedIn = loggedIn;
 module.exports.loggedOut = loggedOut;
+module.exports.notFoundHandler = notFoundHandler;
+module.exports.errorHandler = errorHandler;
