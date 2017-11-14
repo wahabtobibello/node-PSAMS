f5165d25f6a98cfd6187201ee3b66b61ba763504
Tobi Bello <bello.tobi@outlook.com>
2017-11-14 10:30:32 +0100

    used express-validator for login and register routes

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var express = require("express");
+const { body, validationResult } = require("express-validator/check");
 var jwt = require("jsonwebtoken");
 
 var Student = require("../models/Student");
@@ -17,68 +18,110 @@ router
 	.get(middleware.loggedOut, function (req, res) {
 		res.render("login", { csrfToken: req.csrfToken() });
 	})
-	.post(function (req, res, next) {
-		var {
-			matricNumber,
-			password
-		} = req.body;
-		Student.findOne({ matricNumber })
-			.exec(function (err, student) {
-				if (err) return next(err);
-				if (!student) {
-					sendErrorMessage(req, res, "login", "User not found", { csrfToken: req.csrfToken() });
-					return;
-				}
-				student.verifyPassword(password)
-					.then(function (valid) {
-						if (!valid) {
-							sendErrorMessage(req, res, "login", "Username and Password Mismatch", { csrfToken: req.csrfToken() });
-							return;
-						}
-						var token = jwt.sign({
-							exp: Math.floor(Date.now() / 1000) + 3600,
-							nbf: Math.floor(Date.now() / 1000),
-							iss: req.baseUrl,
-							sub: student._id,
-							iat: Math.floor(Date.now() / 1000),
-							is_admin: student.role === "Supervisor"
-						}, process.env.SECRET_KEY);
-						req.session.accessToken = token;
-						res.redirect("/");
-					})
-					.catch(function (err) {
-						next(err);
-					});
-			});
-	});
+	.post(
+		[
+			body("matricNumber")
+				.exists(),
+			body("password")
+				.exists(),
+			body("*")
+				.not()
+				.isEmpty()
+		],
+		function (req, res, next) {
+			var { matricNumber, password } = req.body;
+			const errors = validationResult(req);
+			if (!errors.isEmpty()) {
+				var errorObjs = errors.array();
+				console.log(errorObjs);
+				const errorMsg = errorObjs[0].msg;
+				sendErrorMessage(req, res, "login", errorMsg, { csrfToken: req.csrfToken() });
+				return;
+			}
+
+			Student
+				.findOne({ matricNumber })
+				.exec()
+				.then(function (student) {
+					if (!student) {
+						sendErrorMessage(req, res, "login", "User not found", { csrfToken: req.csrfToken() });
+						return;
+					}
+					student
+						.verifyPassword(password)
+						.then(function (valid) {
+							if (!valid) {
+								sendErrorMessage(req, res, "login", "Username and Password Mismatch", { csrfToken: req.csrfToken() });
+								return;
+							}
+							var token = jwt.sign({
+								exp: Math.floor(Date.now() / 1000) + 3600,
+								nbf: Math.floor(Date.now() / 1000),
+								iss: req.baseUrl,
+								sub: student._id,
+								iat: Math.floor(Date.now() / 1000),
+								is_admin: student.role === "Supervisor"
+							}, process.env.SECRET_KEY);
+							req.session.accessToken = token;
+							res.redirect("/");
+						})
+						.catch(function (err) {
+							next(err);
+						});
+				})
+				.catch(function (err) {
+					next(err);
+				});
+		});
 
 router
 	.route("/register")
 	.get(middleware.loggedOut, function (req, res) {
 		res.render("register", { csrfToken: req.csrfToken() });
 	})
-	.post(function (req, res, next) {
-		var {
-			firstName,
-			lastName,
-			matricNumber,
-			password,
-			confirmPassword,
-		} = req.body;
+	.post(
+		[
+			body("firstName")
+				.exists(),
+			body("lastName")
+				.exists(),
+			body("password")
+				.exists(),
+			body("confirmPassword")
+				.exists()
+				.custom(function (value, { req }) {
+					return value === req.body.password;
+				})
+				.withMessage("Passwords do not match"),
+			body("matricNumber")
+				.exists()
+				.custom(function (matricNumber) {
+					return Student
+						.findOne({ matricNumber })
+						.exec()
+						.then(function (student) {
+							if (student) {
+								throw new Error("User Already Exists");
+							} else {
+								return true;
+							}
+						});
+				}),
+			body("*")
+				.not()
+				.isEmpty(),
+		],
+		function (req, res, next) {
+			var { firstName, lastName, matricNumber, password, } = req.body;
 
-		if (password != confirmPassword) {
-			sendErrorMessage(req, res, "register", "Passwords don't match", { firstName, lastName, matricNumber, csrfToken: req.csrfToken() });
-			return;
-		}
-
-		Student.findOne({
-			matricNumber
-		}).exec(function (err, student) {
-			if (err) return next(err);
-			if (student) {
-				sendErrorMessage(req, res, "register", "User Already Exists", { firstName, lastName, matricNumber, csrfToken: req.csrfToken() });
+			const errors = validationResult(req);
+			if (!errors.isEmpty()) {
+				var errorObjs = errors.array();
+				const errorMsg = errorObjs[0].msg;
+				sendErrorMessage(req, res, "register", errorMsg, { firstName, lastName, matricNumber, csrfToken: req.csrfToken() });
 				return;
 			}
+
 			var newStudentRegistration = new Student({
 				name: {
 					first: firstName,
@@ -101,7 +144,7 @@ router
 				});
 			});
 		});
-	});
+
 router.get("/logout", function (req, res) {
 	req.session = null;
 	res.redirect("/");
