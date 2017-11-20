fddb84681a6c24e5e4143893c41f4703268e8716
Tobi Bello <bello.tobi@outlook.com>
2017-11-20 12:10:10 +0100

    auth test file init setup

---
+++ test/auth.test.js
@@ -0,0 +1,45 @@
+const supertest = require("supertest");
+const cheerio = require("cheerio");
+const app = require("../app");
+
+describe("register route", () => {
+  const request = supertest.agent("http://localhost:3000");
+  let csrfValue;
+  test("It should response the GET method", () => {
+    return request.get("/register")
+      .then(response => {
+        const $ = cheerio.load(response.text);
+        csrfValue = $("input[name='_csrf']").val();
+        expect(response.statusCode).toBe(200);
+        expect(response.get("Set-Cookie")).toBeTruthy();
+      });
+  });
+
+  test("It should response the POST method", () => {
+    return request.post("/register")
+      .send({
+        firstName: "Tobi",
+        lastName: "Bello",
+        matricNumber: 130805001,
+        password: "nutella",
+        confirmPassword: "nutella",
+        _csrf: csrfValue
+      })
+      .then(response => {
+        expect(response.statusCode).toBe(200);
+        expect(response.redirect).toBeTruthy();
+
+      });
+  });
+
+  // test("It should response the GET method", () => {
+  //   return request.get("/login").then(response => {
+  //     // for (let key in response) {
+  //     //   console.log(key);
+  //     // }
+  //     const $ = cheerio.load(response.text);
+  //     console.log($("input[name='_csrf']").val());
+  //     expect(response.statusCode).toBe(200);
+  //   });
+  // });
+});
